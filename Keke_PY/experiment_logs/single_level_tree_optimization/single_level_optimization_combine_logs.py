0c5f8ae1e468434cae8481af1f5ba8b28c172a01
Robert Jesse Ronge <robert.jesse.ronge@gmail.com>
2025-03-07 17:35:37 +0100

    long 100 generations single level training runs archived

--- Keke_PY/experiment_logs/single_level_tree_optimization/single_level_optimization_combine_logs.py
+++ Keke_PY/experiment_logs/single_level_tree_optimization/single_level_optimization_combine_logs.py
@@ -7,7 +7,7 @@ from Keke_PY.heuristic_pymoo_representations.HeuristicTreeRepresentation import 
 from Keke_PY.heuristic_pymoo_representations.TrackedRepresentation import TrackedRepresentation
 from Keke_PY.keke_game.simulation import load_level_set
 
-log_location: str = "Keke_PY/experiment_logs/single_level_tree_optimization/single_training_logs"
+log_location: str = "Keke_PY/experiment_logs/single_level_tree_optimization/long_100_gens_single_training_logs"
 
 levels: List[str] = [
     *[level["ascii"] for level in
@@ -17,8 +17,8 @@ levels: List[str] = [
 ]
 
 def get_level_results(level_nr: int) -> KekeProblem:
-    slurm_job_id: int = 523700 if level_nr != 183 else 524904
-    file_name: str = f"keke_single_level_training_{slurm_job_id}_{level_nr}-out.txt"
+    slurm_job_id: int = 553991 # 523700 if level_nr != 183 else 524904
+    file_name: str = f"keke_long_single_level_training_{slurm_job_id}_{level_nr}-out.txt"
     with open(pathlib.Path(log_location, file_name)) as file:
         lines: [str] = file.readlines()
     representation: TrackedRepresentation = TrackedRepresentation(
@@ -33,25 +33,27 @@ def get_level_results(level_nr: int) -> KekeProblem:
     print(f"reading in level {level_nr + 1} of {len(levels)} done.")
     return testing_data
 
-
+def get_evaluation_str(level_nr: int) -> List[str]:
+    testing_data: KekeProblem = get_level_results(level_nr)
+    evaluations: List[str] = []
+    for lvl in levels:
+        evaluation: Tuple[Optional[List[str]], int] = testing_data.past_evaluations_by_gen_index_and_level_id[
+            (0, 0, testing_data.level_to_id_map[lvl])
+        ]
+        if evaluation[0] is None:
+            evaluations.append("----")
+        else:
+            evaluations.append(str(evaluation[1]))
+    return evaluations
 
 if __name__ == '__main__':
 
     print("\n---READING IN DATA---\n")
 
-    level_data_list: List[KekeProblem] = multiprocessing.Pool(8).map(
-        get_level_results, range(len(levels))
+    evaluations_list: List[List[str]] = multiprocessing.Pool(6).map(
+        get_evaluation_str, range(len(levels))
     )
 
     print("\n---EVALUATION---\n")
-    for result in level_data_list:
-        evaluations: List[str] = []
-        for lvl in levels:
-            evaluation: Tuple[Optional[List[str]], int] = result.past_evaluations_by_gen_index_and_level_id[
-                (0, 0, result.level_to_id_map[lvl])
-            ]
-            if evaluation[0] is None:
-                evaluations.append("----")
-            else:
-                evaluations.append(str(evaluation[1]))
+    for evaluations in evaluations_list:
         print("LEVEL AGENT EVALUATION:" + '\t:'.join(evaluations))
