fb67bcc1c0b9ee4fd50620d382ad3dfefa274435
Robert Jesse Ronge <robert.jesse.ronge@gmail.com>
2025-03-11 12:31:55 +0100

    cross level evaluation visualization

--- Keke_PY/evaluation/single_level_optimization_minimal_solving_set.py
+++ Keke_PY/evaluation/single_level_optimization_minimal_solving_set.py
@@ -1,6 +1,11 @@
 from typing import List
 
-with open("Keke_PY/evaluation/single_level_optimization_combined_logs.txt") as file:
+file_name: str = [
+    "Keke_PY/experiment_logs/single_level_tree_optimization/single_level_optimization_combined_logs.txt",
+    "Keke_PY/experiment_logs/single_level_tree_optimization/long_100_gens_single_level_optimization_combined_logs.txt",
+][1]
+
+with open(file_name) as file:
     lines: List[str] = file.readlines()
 
 
@@ -14,3 +19,14 @@ for line in lines:
 
 for row in solving_matrix:
     print(*row)
+
+import numpy as np
+a = np.array(solving_matrix)
+import matplotlib.pyplot as plt
+
+plt.imshow(a)
+plt.show()
+
+print(np.sum(np.max(a, axis=0)))
+print(a.shape)
+print(148/184)
