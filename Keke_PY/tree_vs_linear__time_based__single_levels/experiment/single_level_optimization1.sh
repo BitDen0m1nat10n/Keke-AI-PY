447de21388b0fac23c76580bcc031ff9874c731d
Robert Jesse Ronge <robert.jesse.ronge@gmail.com>
2025-05-17 20:01:55 +0200

    split batch script

---
+++ Keke_PY/tree_vs_linear__time_based__single_levels/experiment/single_level_optimization1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name=KekeTimeBasedSingleLevelOptimization50Gens
+#SBATCH --mail-user=rongero@tnt.uni-hannover.de
+#SBATCH --mail-type=ALL
+#SBATCH --partition=cpu_short_stud
+
+#SBATCH --array=0-99 # 184lvl * (linear + trees) = 0-367
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=2:00:00 # expected max time: (baseline 184min + train 500min + eval 184min = 868min) / 10cpus = 90min
+#SBATCH --output=KekeTimeBasedSingleLevelOptimization50Gens_%A_%a-out.txt   # Logdatei für den merged STDOUT/STDERR output (%A wird durch slurm Job-ID ersetzt und %a durch den Array Index)
+
+
+# setup conda, and a conda-environment like environment.txt :
+#source setup_on_my_laptop.sh
+source setup_on_cluster.sh
+
+
+
+# Change to my work dir
+# SLURM_SUBMIT_DIR is an environment variable that automatically gets
+# assigned the directory from which you did submit the job. A batch job
+# is like a new login, so you'll initially be in your HOME directory.
+# So it's usually a good idea to first change into the directory you did
+# submit your job from.
+cd $SLURM_SUBMIT_DIR
+
+python Keke_PY/tree_vs_linear__time_based__single_levels/experiment/single_level_optimization.py --argument $SLURM_ARRAY_TASK_ID
