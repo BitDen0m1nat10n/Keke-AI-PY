2829871a4dea2e7b4ca786600bb9cb9c381037e3
Robert Jesse Ronge <robert.jesse.ronge@gmail.com>
2025-05-26 19:26:38 +0200

    EnsembleProblem created to train Classifiers on the minimal solving set; not tested yet!

--- Keke_PY/tree_vs_linear__time_based__single_levels/evaluation/minimal_solving_set.py
+++ Keke_PY/tree_vs_linear__time_based__single_levels/evaluation/minimal_solving_set.py
@@ -1,27 +1,51 @@
-from typing import List
-import pulp
+from concurrent.futures import ProcessPoolExecutor
+from copy import deepcopy
+from typing import List, Tuple, Dict, Optional, Union
 
+import numpy as np
+import pulp
+from pymoo.core.problem import Problem
+
+from Keke_PY.experiments.KekeProblem import KekeProblem
+from Keke_PY.heuristic_pymoo_representations.HeuristicRepresentation import HeuristicRepresentation
+from Keke_PY.heuristic_pymoo_representations.TupleRepresentation import TupleRepresentation
+from Keke_PY.heuristics.ParametrisedHeuristic import Heuristic
+from Keke_PY.keke_game.keke import GameState, make_level, parse_map
 from Keke_PY.keke_game.simulation import load_level_set
+from Keke_PY.search_agents.HeuristicGuidedSearch import HeuristicGuidedSearch
 
 file_name: str = "Keke_PY/tree_vs_linear__time_based__single_levels/experiment_logs/combined_logs.txt"
+
+all_levels: List[str] = [
+    *[level["ascii"] for level in
+      load_level_set("./json_levels/train_LEVELS.json")["levels"]],
+    *[level["ascii"] for level in
+      load_level_set("./json_levels/test_LEVELS.json")["levels"]],
+]
 
 with open(file_name) as file:
     lines: List[str] = file.readlines()
 
 
 solving_matrix: List[List[bool]] = []
+genome_list: List[str] = []
 
 for line in lines:
     if line.startswith("LEVEL AGENT EVALUATION"):
         _, *results = line.split(':')
         solving_matrix.append([result.strip() != "----" for result in results])
+    if line.startswith("LEVEL AGENT GENOME"):
+        _, *result = line.split(':')
+        genome_list.append(':'.join(result))
 
 levels: range = range(len(solving_matrix) // 2)
 
 linear_solving_matrix: List[List[bool]] = [solving_matrix[2 * i] for i in levels]
 tree_solving_matrix: List[List[bool]] = [solving_matrix[2 * i + 1] for i in levels]
 
-solving_matrix: List[List[bool]] = tree_solving_matrix # TODO: document
+use_trees: bool = True # TODO: document
+
+solving_matrix: List[List[bool]] = tree_solving_matrix if use_trees else linear_solving_matrix
 training_level_count: int = len(load_level_set("./json_levels/train_LEVELS.json")["levels"])
 
 
@@ -34,6 +58,12 @@ if reduce_to_training_levels:
 
 
 
+
+
+
+
+
+# Get Minimal-Solving-Set
 # The following code is a modified version of https://github.com/AlbrErik/bachelor-thesis/blob/main/KekeCompetition-main/OptimizingKekeAgents/evaluation/min_heu_module.py
 
 problem = pulp.LpProblem("Minimal_Solving_Set", pulp.LpMinimize)
@@ -52,7 +82,6 @@ problem += pulp.lpSum(use_heuristic)
 for lvl in levels:
     if not any(heur[lvl] for heur in solving_matrix):
         continue
-    print(lvl)
     problem += pulp.lpSum(
         heur_used
         for i, heur_used in enumerate(use_heuristic)
@@ -70,11 +99,99 @@ print(minimal_solving_set)
 all_solved_levels: List[bool] = [any(solving_matrix[e][lvl] for e in minimal_solving_set) for lvl in range(len(solving_matrix[0]))]
 
 for heur in minimal_solving_set:
-    print(f'heuristic{heur}:\t' + ''.join('1' if solving_matrix[heur][lvl] else '0' for lvl in range(len(solving_matrix[0]))))
+    print(f'heur{heur}\t:\t' + ''.join('1' if solving_matrix[heur][lvl] else '0' for lvl in range(len(solving_matrix[0]))))
+    genome_index: int = 2 * heur + (1 if use_trees else 0)
+    print(genome_list[genome_index])
 
-print('solved levels:\t' + ''.join('1' if all_solved_levels[lvl] else '0' for lvl in range(len(solving_matrix[0]))))
-print('solvable levels:\t'.join('1' if solvable_levels[lvl] else '0' for lvl in range(len(solving_matrix[0]))))
+print('solved  :\t' + ''.join('1' if all_solved_levels[lvl] else '0' for lvl in range(len(solving_matrix[0]))))
+print('solvable:\t' + ''.join('1' if solvable_levels[lvl] else '0' for lvl in range(len(solving_matrix[0]))))
 
 print(f"{sum(all_solved_levels)}/{len(solving_matrix[0])} levels solved")
 
 
+
+
+
+
+# Train Ensemble
+"""
+def get_first_heuristic_value(serialized_heuristic: str, level_nr: int) -> float:
+    representation: HeuristicRepresentation = HeuristicTreeRepresentation() if use_trees else WeightedHeuristicSumRepresentation()
+    heuristic: Heuristic = representation.into_heuristic(representation.deserialize(serialized_heuristic))
+    initial_state: GameState = make_level(parse_map(all_levels[level_nr]))
+    ctx = {
+        "initial GameState": initial_state,
+        "initial rules": set(initial_state.rules)
+    }
+    return heuristic.run(initial_state, ctx)
+"""
+
+class EnsembleProblem(KekeProblem):
+
+    ensemble_parts: int
+    tuple_representation: TupleRepresentation
+    inner_representation: HeuristicRepresentation
+    level_and_times_for_policy: List[Tuple[str, Dict[str, Optional[float]]]]
+
+    def __init__(
+            self,
+            representation: HeuristicRepresentation,
+            level_and_times_for_policy: List[Tuple[str, Dict[str, Optional[float]]]],
+    ):
+        self.inner_representation = representation
+        self.ensemble_parts = len(level_and_times_for_policy)
+        self.tuple_representation = TupleRepresentation(representation, self.ensemble_parts)
+        self.level_and_times_for_policy = level_and_times_for_policy
+        problem_data: Problem = representation.get_problem_data()
+        super().__init__(
+            self,
+            training_batches = [level_and_times_for_policy[0][1].keys()],
+            representation = self.tuple_representation,
+            max_node_expansions = None,
+            max_calculation_time = 60.0,
+            time_dependent_performance_function = False,
+            executor = ProcessPoolExecutor(),
+            test_batch = (),
+            agent_factory = HeuristicGuidedSearch.GuidedSearchFactory(),
+            silent = False,
+            logging_prefix = "",
+        )
+
+    def register_and_run_next_generation(self, instances: [np.ndarray]):
+
+        if self.is_continuation(instances):
+            self.log_line("RESTART FROM PREVIOUS GENERATION DETECTED => SIMULATION WILL BE SKIPPED; RESULTS FROM PREVIOUS SIMULATION ARE RETURNED TO CALLER")
+            self.try_continue_from_last_generation = False
+            return
+
+        self.log_generation_data(list(instances))
+        self.past_instances_by_gen_and_index.update(((self.generation, index), deepcopy(instance)) for index, instance in enumerate(instances))
+
+        self.fake_run_as_next_generation([
+            self.tuple_representation.into_heuristics(instance)
+            for instance in instances
+        ])
+
+    def fake_run_as_next_generation(self, instances: List[List[Heuristic]]):
+        simulation_results: Dict[Tuple[int, str], Tuple[Union[List[str], None], int, float]] = {}
+        for level_str in self.all_levels:
+            initial_level_state: GameState = make_level(parse_map(level_str))
+            ctx = {
+                "initial GameState": initial_level_state,
+                "initial rules": set(initial_level_state.rules)
+            }
+            for i, ensemble_parts in enumerate(instances):
+                priorities: List[float] = [
+                    classifier_part.run(initial_level_state, ctx)
+                    for classifier_part in ensemble_parts
+                ]
+                chosen_policy: int = priorities.index(max(priorities))
+                stored_solving_time: Optional[float] = self.level_and_times_for_policy[chosen_policy][1][level_str]
+                fake_solution: Optional[List[str]] = None if stored_solving_time is None else ["NOT STORED"] # TODO: log real solution
+                fake_node_expansions: int = None # TODO: log real node_expansions
+                simulation_results[(i, level_str)] = (fake_solution, fake_node_expansions, stored_solving_time)
+
+        self.past_evaluations_by_gen_index_and_level_id.update(((self.generation, key[0], self.level_to_id_map[key[1]]), result) for key, result in simulation_results.items())
+        self.log_simulation_data(simulation_results)
+
+        self.generation += 1
