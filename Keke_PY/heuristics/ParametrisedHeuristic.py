38a3db0c2b285f7e1dd5f3caa6e2bfc15eef7b15
Robert Jesse Ronge <robert.jesse.ronge@gmail.com>
2025-05-28 15:18:10 +0200

    random forest classifier implemented

--- Keke_PY/heuristics/ParametrisedHeuristic.py
+++ Keke_PY/heuristics/ParametrisedHeuristic.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from collections.abc import Callable
 from inspect import signature
-from typing import final
+from typing import final, Tuple
 
 from Keke_PY.keke_game.keke import GameState
 
@@ -20,6 +20,9 @@ class ParametrisedHeuristic(ABC):
     @staticmethod
     def from_func(heuristic: Callable, override_nr_of_parameters: int = -1) -> 'ParametrisedHeuristicFromCallable':
         return ParametrisedHeuristicFromCallable(heuristic, override_nr_of_parameters)
+
+    def with_parameters(self, *parameters: float) -> 'ParametrisedHeuristicWithParameters':
+        return ParametrisedHeuristicWithParameters(self, *parameters)
 
 
 class Heuristic(ParametrisedHeuristic, ABC):
@@ -48,3 +51,17 @@ class ParametrisedHeuristicFromCallable(ParametrisedHeuristic):
     def run(self, state: GameState, ctx: dict, *args: float) -> float:
         return float(self.heuristic_callable(state, ctx, *args))
 
+@dataclass
+class ParametrisedHeuristicWithParameters(Heuristic):
+    parametrised_heuristic: ParametrisedHeuristic
+    parameters: Tuple[float, ...]
+
+    def __init__(self, parametrised_heuristic: ParametrisedHeuristic, *parameters: float):
+        assert parametrised_heuristic.nr_of_parameters == len(parameters), \
+            f"Heuristic needs {parametrised_heuristic.nr_of_parameters} parameters. {len(parameters)} were given."
+        self.parametrised_heuristic = parametrised_heuristic
+        self.parameters = parameters
+
+    def run(self, state: GameState, ctx: dict, *args: float) -> float:
+        assert len(args) == 0, f"No parameters are expected. {args} are not supposed to be here."
+        return self.parametrised_heuristic.run(state, ctx, *self.parameters)
--- Keke_PY/tree_vs_linear__time_based__single_levels/ensemble/random_forest_classifier_ensemble.py
+++ Keke_PY/tree_vs_linear__time_based__single_levels/ensemble/random_forest_classifier_ensemble.py
@@ -1,16 +1,26 @@
-from typing import List, Optional
+import math
+from random import shuffle
+from typing import List, Optional, Tuple, Dict
 
+import numpy as np
 import pulp
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+from Keke_PY.heuristics.ParametrisedHeuristic import Heuristic
+from Keke_PY.heuristics.hand_crafted_heuristics import named_heuristics
+from Keke_PY.keke_game.keke import parse_map, make_level, GameState
 from Keke_PY.keke_game.simulation import load_level_set
 
 file_name: str = "Keke_PY/tree_vs_linear__time_based__single_levels/experiment_logs/combined_logs.txt"
 
-all_levels: List[str] = [
-    *[level["ascii"] for level in
-      load_level_set("./json_levels/train_LEVELS.json")["levels"]],
-    *[level["ascii"] for level in
-      load_level_set("./json_levels/test_LEVELS.json")["levels"]],
+training_levels: List[str] = [
+    level["ascii"] for level in load_level_set("./json_levels/train_LEVELS.json")["levels"]
 ]
+testing_levels: List[str] = [
+    level["ascii"] for level in load_level_set("./json_levels/test_LEVELS.json")["levels"]
+]
+all_levels: List[str] = [*training_levels, *testing_levels]
 
 with open(file_name) as file:
     lines: List[str] = file.readlines()
@@ -29,6 +39,15 @@ for line in lines:
         _, *result = line.split(':')
         genome_list.append(':'.join(result))
 
+times_by_genome_str_and_level_str: Dict[Tuple[str, str], Optional[float]] = dict([
+    ((genome_str, test_level), time_matrix[instance_i][test_i])
+    for instance_i, genome_str in enumerate(genome_list)
+    for test_i, test_level in enumerate(all_levels)
+])
+
+linear_genome_strs: List[str] = genome_list[0::2]
+tree_genome_strs: List[str] = genome_list[1::2]
+
 levels: range = range(len(solving_matrix) // 2)
 
 linear_solving_matrix: List[List[bool]] = [solving_matrix[2 * i] for i in levels]
@@ -37,6 +56,7 @@ tree_solving_matrix: List[List[bool]] = [solving_matrix[2 * i + 1] for i in leve
 use_trees: bool = True # TODO: document
 
 solving_matrix: List[List[bool]] = tree_solving_matrix if use_trees else linear_solving_matrix
+genome_strs: List[str] = tree_genome_strs if use_trees else linear_genome_strs
 training_level_count: int = len(load_level_set("./json_levels/train_LEVELS.json")["levels"])
 
 
@@ -100,5 +120,113 @@ print('solvable:\t' + ''.join('1' if solvable_levels[lvl] else '0' for lvl in ra
 print(f"{sum(all_solved_levels)}/{len(solving_matrix[0])} levels solved")
 
 
+
+
+
+minimal_solving_set_genome_strs: List[str] = [genome_strs[index] for index in minimal_solving_set]
+
+
+# build level features and ideal classifications
+
+level_feature_names_and_params: List[Tuple[str, Tuple[float, ...]]] = [
+    ("number_of_goal_objects", ()),
+    ("number_of_player_objects", ()),
+    ("connectivity", ()),
+    ("number_of_auto_movers", ()),
+
+    ("number_of_stuck_is_words", ()),
+    ("number_of_stuck_prefixes", ()),
+    ("number_of_stuck_suffixes", ()),
+    ("number_of_stuck_important_suffixes", ()),
+
+    ("number_of_killer_objects", ()),
+    ("number_of_pushable_objects", ()),
+    ("number_of_sinkable_objects", ()),
+    ("number_of_stopped_objects", ()),
+    ("player_killer_distance", (-1.0,)),
+
+    ("distance_to_winnable_objects", (-1.0,)),
+    ("distance_to_words", (-1.0,)),
+    ("distance_to_pushable_objects", (-1.0,)),
+
+    ("number_of_newly_created_rules", ()),
+
+    ("goal_reachability", ())
+]
+
+named_level_feature_heuristics: Dict[str, Heuristic] = dict([
+    (name, dict(named_heuristics)[name].with_parameters(*parameters))
+    for name, parameters in level_feature_names_and_params
+])
+
+def get_feature_value(level_str: str, feature_heuristic: Heuristic) -> float:
+    initial_level_state: GameState = make_level(parse_map(level_str))
+    ctx: dict = {
+        "initial GameState": initial_level_state,
+        "initial rules": set(initial_level_state.rules)
+    }
+    return feature_heuristic.run(initial_level_state, ctx)
+
+feature_by_level_and_name: Dict[Tuple[str, str], float] = dict([
+    ((level_str, feature_name), get_feature_value(level_str, feature_heuristic))
+    for level_str in all_levels for feature_name, feature_heuristic in named_level_feature_heuristics.items()
+])
+
+def get_best_genome_of_set_on_level_str(genome_str_set: List[str], level_str: str) -> str:
+    genome_str_set = list(genome_str_set) # to make sure, this is actually a list and not a set o.e.
+    times: List[Optional[float]] = [
+        times_by_genome_str_and_level_str[(genome_str, level_str)]
+        for genome_str in genome_str_set
+    ]
+    penalty: List[float] = [math.inf if time is None else time for time in times]
+    return genome_str_set[penalty.index(min(*penalty))]
+
+best_genome_of_minimal_solving_set_label_for_level_str: Dict[str, str] = dict([
+    (level_str, get_best_genome_of_set_on_level_str(minimal_solving_set_genome_strs, level_str))
+    for level_str in all_levels
+])
+
+def get_scikit_learn_data_for_level_strs(level_strs: List[str]) -> Tuple[np.ndarray, np.ndarray]:
+    level_strs = list(level_strs) # to make sure, this is actually a list and not a set o.e.
+    # {array-like, sparse matrix} of shape (n_samples, n_features)
+    x_values: np.ndarray = np.array([
+        [
+            feature_by_level_and_name[(level_str, feature_name)]
+            for feature_name, _ in level_feature_names_and_params
+        ]
+        for level_str in level_strs
+    ])
+    y_values: List[str] = [
+        best_genome_of_minimal_solving_set_label_for_level_str[level_str]
+        for level_str in level_strs
+    ]
+    #shuffle(y_values)
+    return x_values, np.array(y_values)
+
+train_x, train_y = get_scikit_learn_data_for_level_strs(training_levels)
+test_x, y_test = get_scikit_learn_data_for_level_strs(testing_levels)
+
+classifier: RandomForestClassifier = RandomForestClassifier()
+
+classifier.fit(train_x, train_y)
+
+y_pred = classifier.predict(test_x)
+
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+solvable_test_level_count: int = len([
+    test_level for test_level in testing_levels
+    if any(
+        times_by_genome_str_and_level_str[(genome_str, test_level)] is not None
+        for genome_str in minimal_solving_set_genome_strs
+    )
+])
+solved_test_level_count: int = len([
+    test_level for i, test_level in enumerate(testing_levels)
+    if times_by_genome_str_and_level_str[(y_pred[i], test_level)] is not None
+])
+print(f"{solved_test_level_count}/{solvable_test_level_count}/{len(testing_levels)}")
+#print(y_pred)
 
 
